f522171c9ed7dcbeccb4f0dad7102ded111797df
zjaacmyx <zhifengaac@gmail.com>
2025-11-18 20:02:39 +0800

    Update setup_proxy_dns.sh

--- setup_proxy_dns.sh
+++ setup_proxy_dns.sh
@@ -42,7 +42,7 @@ echo
 echo "[*] 正在更新软件包列表并安装必要的系统依赖..."
 apt update -y > /dev/null 2>&1
 apt install -y python3 python3-pip curl socat > /dev/null 2>&1
-pip3 install requests --quiet --disable-pip-version-check --no-python-version-warning
+apt install -y python3 python3-requests python3-pip curl socat > /dev/null 2>&1
 echo "[√] 系统依赖安装完成。"
 echo
 
